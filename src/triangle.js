acf6d205455321757ad0a2633fa018f88d231c3f
delcidj22 <jdelcdid23@gmail.com>
2021-12-20 19:39:01 -0800

    add test 2 - not a triangle

--- src/triangle.js
+++ src/triangle.js
@@ -2,4 +2,9 @@ export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
+  this.isTriangle = ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) ? "not a triangle" : "this is a triangle"; 
+}
+
+Triangle.prototype.checkType = function() {
+  return "not a triangle";
 }
