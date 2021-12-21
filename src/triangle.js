cec1430eb6dd9f0cdec4d338209c93dd9d439cf9
delcidj22 <jdelcdid23@gmail.com>
2021-12-20 20:34:16 -0800

    add test 5 - equilateral triangle

--- src/triangle.js
+++ src/triangle.js
@@ -2,7 +2,6 @@ export default function Triangle(side1, side2, side3) {
   this.side1 = side1;
   this.side2 = side2;
   this.side3 = side3;
-  this.isTriangle = ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) ? "not a triangle" : "this is a triangle"; 
 }
 
 Triangle.prototype.checkType = function() {
@@ -10,7 +9,9 @@ Triangle.prototype.checkType = function() {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
-  }  else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
-      return "isosceles triangle";
-    }
+  } else if ((this.side1 === this.side2) && (this.side1 === this.side3)) {
+      return "equilateral triangle";
+  } else {
+    return "isosceles triangle";
+  }
 };
